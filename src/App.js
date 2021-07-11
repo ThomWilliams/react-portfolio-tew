009927c5d6c903a4e72c3fa144d17e83b36e28ea
Thom Williams <thomwilliams1990@gmail.com>
2021-07-11 17:58:40 +0100

    files restructured

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import './app.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
+import Project from './components/Project';
 
 function App() {
   return (
