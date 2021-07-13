cbd0fc7d81c992dafcd719bc46fdf41f1b6974d3
Thom Williams <thomwilliams1990@gmail.com>
2021-07-13 22:44:08 +0100

    footer finalised

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import linkedinLogo from "../images/linked_in.png";
+import githubLogo from "../images/GitHub-Mark.png";
 
 function Footer() {
-  const message = 'The Footer';
+  const message = "The Footer";
   return (
     <div className="footer">
-      <h4>{message}</h4>
+      <div className="image-container">
+        <a href="https://www.linkedin.com/in/thom-williams-624408205/">
+          <img src={linkedinLogo} alt="Logo" />
+        </a>
+        <a href="https://github.com/ThomWilliams">
+          <img src={githubLogo} alt="Logo" />
+        </a>
+      </div>
     </div>
   );
 }
