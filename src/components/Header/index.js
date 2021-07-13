529a4d7cfe28d872a07de5aca03e9f364bfb02ae
Thom Williams <thomwilliams1990@gmail.com>
2021-07-13 22:58:54 +0100

    header and navbar styling updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './header.css';
 
 function Header() {
-  const message = 'This is the Header';
+  const h1message = 'Thom Williams';
+  const h2message = 'Porfolio Site';
   return (
     <header className="header">
-      <h1>{message}</h1>
+      <h1>{h1message}</h1>
+      <h2>{h2message}</h2>
     </header>
   );
 }
