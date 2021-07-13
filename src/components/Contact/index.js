51e5a4feba2556d78ef78143d675b210bef12012
Thom Williams <thomwilliams1990@gmail.com>
2021-07-13 23:41:21 +0100

    contact form added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,50 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './contact.css'
 
-function Project() {
-  const message = 'This is the projects Component';
+
+function Form() {
+  // Here we set two state variables for firstName and lastName using `useState`
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'firstName') {
+      setFirstName(inputValue);
+    }  else if (inputType === 'lastName') {
+        setLastName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // Alert the user their first and last name, clear the inputs
+    alert(`Hello ${firstName} ${lastName}`);
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
-    <section className="content-container">
-      <h2>{message}</h2>
-    </section>
+    <div>
+      <p>
+        Contact Thom Williams by filling out the following form:
+      </p>
+      <form className="form">
+        <input
+          value={firstName}
+          name="firstName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          value={lastName}
+          name="lastName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Last Name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="email"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
-export default Project;
+export default Form;
 
-
-// CARDS
-
-
-// import React from 'react';
-
-// export default function Card(props) {
-//   const cardStyle = {
-//     width: '18rem',
-//   };
-
-//   // Helper function that generates a random width for our placeholder images
-//   const randomWidth = () => {
-//     const number = Math.random() * (300 - 200) + 200;
-//     return number.toString().split('.')[0];
-//   };
-
-//   return (
-//     <div>
-//       <div className="card" style={cardStyle}>
-//         <img
-//           className="card-img-top"
-//           src={`http://placecorgi.com/${randomWidth()}`}
-//           alt="Card cap"
-//         />
-//         <div className="card-body">
-//           <h5 className="card-title">{props.name}</h5>
-//           <p className="card-text">{props.description}</p>
-//           <a href="#" className="btn btn-primary">
-//             Adopt {props.name}
-//           </a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
